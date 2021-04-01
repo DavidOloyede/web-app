50366de46a8c854e9105da656f3d19c2827c703e
David Oloyede <54964087+DavidOloyede@users.noreply.github.com>
2021-04-01 15:26:01 -0500

    Add files via upload

---
+++ static/js/scripts.js
@@ -0,0 +1,20 @@
+$("form[name=login").submit(function (e) {
+  var $form = $(this);
+  var $error = $form.find(".error");
+  var data = $form.serialize();
+
+  $.ajax({
+    url: "/user/login",
+    type: "POST",
+    data: data,
+    dataType: "json",
+    success: function (resp) {
+      window.location.href = "/profile/";
+    },
+    error: function (resp) {
+      $error.text(resp.responseJSON.error).removeClass("error--hidden");
+    },
+  });
+
+  e.preventDefault();
+});
